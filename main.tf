3f0b8a104a885afa906ac08c9f93664db8836327
michael lindsey <kennedyxiv@icloud.com>
2022-10-29 02:59:36 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+
+    tags = {
+      "Name" = "terraform-example"
+    }
+  
+
+}
